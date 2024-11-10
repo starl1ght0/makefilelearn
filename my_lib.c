05420aa1a9c255ca716349c96d096e652fd5f381
starl1ght0 <st4rl1ght0s@gmail.com>
2024-11-10 17:03:24 +0700

    firstcommit

---
+++ my_lib.c
@@ -0,0 +1,11 @@
+
+
+
+#ifndef MY_LIB_C
+#define MY_LIB_C
+
+int foo(int x ) {
+    return x * x;
+}
+
+#endif
---
+++ my_lib.h
@@ -0,0 +1,11 @@
+
+
+
+#ifndef MY_LIB_H
+#define MY_LIB_H
+
+int foo(int x);
+
+#endif
+
+
